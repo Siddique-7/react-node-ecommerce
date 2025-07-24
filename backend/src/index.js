b56b8daa44204c521fd219e3f8f3d6db59f22bd2
Siddique-7 <muhammadsiddik687@gmail.com>
2025-07-24 14:33:17 +0530

    Update: removed PORT from .env for Render deployment

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import connectDB from "./db/connectDB.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
